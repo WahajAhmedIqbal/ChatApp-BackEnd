94a916f099cd796fb1eb9805620547a45d60ec37
WahajAhmedIqbal <wahaj465@gmail.com>
2021-12-11 01:15:51 +0500

    working on on/of

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const { Server } = require('socket.io')
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+const users = {}
+
+io.on('connection', (socket) => {
+    console.log("user connecte", socket.id)
+
+    socket.on('send_message', (data) => {
+        console.log(data)
+        // const senderId = data.senderId
+        // io.to(data.senderId).emit('receive_message', data)
+        io.emit('receive_message', data);
+        // socket.to(data.senderId).emit('receive_message', data)
+    })
+
+    // socket.on('login', (data) => {
+    //     console.log('a user ' + data.userId + " connected")
+    //     socket.on('login').emit(data.userId)
+    //     users[socket.id] = data.userId
+    //     console.log(users)
+    // })
+
+
+    // socket.on("disconnect", () => {
+    //     console.log('user ' + users[socket.id] + " disconnected")
+    //     delete users[socket.id]
+    // })
+})
+
+
+const port = 3001
+
+server.listen(port, () => {
+    console.log('server is listing on ', port)
+})
